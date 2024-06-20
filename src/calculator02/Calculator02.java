71f0c883b8ad9f9d78b6c1ddde75d793d1517a70
Aluno <Aluno@LAB04-PC09>
2024-06-19 21:36:11 -0300

    Commit project Calculator02

---
+++ src/calculator02/Calculator02.java
@@ -0,0 +1,10 @@
+
+package Calculator02;
+
+
+public class Calculator02 {
+    public static double findTotal(double person) {
+       return person * (1 + 0.05 + 0.15);
+    }
+    
+}
---
+++ src/calculator02/CalculatorTest2.java
@@ -0,0 +1,46 @@
+package Calculator02;
+
+import Calculator02.Calculator02;
+
+public class CalculatorTest2 {                                
+    public static void main(String[] args) { 
+        double person1 = 10;
+        System.out.println(Calculator02.findTotal(person1));
+        
+       double person2 = 12;
+       System.out.println(Calculator02.findTotal(person2));
+       
+       double person3 = 9;
+       System.out.println(Calculator02.findTotal(person3));
+       
+       double person4 = 8;
+       System.out.println(Calculator02.findTotal(person4));   
+       
+       double person5 = 7;
+       System.out.println(Calculator02.findTotal(person5));
+        
+       double person6 = 15;
+       System.out.println(Calculator02.findTotal(person6));
+       
+       double person7 = 11;
+       System.out.println(Calculator02.findTotal(person7));
+
+       double person8 = 30;
+       System.out.println(Calculator02.findTotal(person8));
+
+       /*Valor antes das taxas:
+        Person 1: $10
+        Person 2: $12
+        Person 3: $9
+        Person 4: $8
+        Person 5: $7
+        Person 6: $15 (Alex)
+        Person 7: $11
+        Person 8: $30
+        */
+    }
+    
+}
+        
+        
+      
